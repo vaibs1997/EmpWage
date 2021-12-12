ca77c4cdddc912e0c89f1fdc368c8417df66cc22
vaibs <vaibhavmuratkar2@gmail.com>
2021-12-12 13:20:39 +0530

    Monthly EmpWage UC5

--- src/com/bridgelabz/basiccoreprogram/Empwage.java
+++ src/com/bridgelabz/basiccoreprogram/Empwage.java
@@ -4,28 +4,35 @@ public class Empwage {
     public static void main(String[] args) {
         //Constants
         final int IS_FULL_TIME = 1;
-        final int IS_PART_TIME =2;
-        final int WAGE_PER_HR =20;
+        final int IS_PART_TIME = 2;
+        final int WAGE_PER_HR = 20;
+        final int NUMBER_OF_DAYS = 20;
         //Computation
-        int empHrs;
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        int empHrs, totalWage=0, empWage;
+        for (int day=0; day < NUMBER_OF_DAYS; day++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-        switch (empCheck) {
-            case IS_FULL_TIME -> {
-                System.out.println("Employee is Full Time");
-                empHrs = 8;
+            switch (empCheck) {
+                case IS_FULL_TIME -> {
+                    System.out.println("Employee is Full Time");
+                    empHrs = 8;
+                }
+                case IS_PART_TIME -> {
+                    System.out.println("Employee is Part Time");
+                    empHrs = 4;
+                }
+                default -> {
+                    System.out.println("Employee is Absent");
+                    empHrs = 0;
+                }
             }
-            case IS_PART_TIME -> {
-                System.out.println("Employee is Part Time");
-                empHrs = 4;
-            }
-            default -> {
-                System.out.println("Employee is Absent");
-                empHrs = 0;
-            }
+            empWage = empHrs * WAGE_PER_HR;
+            totalWage += empWage;
+            System.out.println(" EMP Wage: " + empWage);
         }
 
-        int totalWage = empHrs * WAGE_PER_HR;
-        System.out.println(totalWage);
+
+
+        System.out.println("Total Wage: " + totalWage);
     }
 }
