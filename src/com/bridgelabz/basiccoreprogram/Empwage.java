9c76432c062e19fa66fe05c368926db33966adfd
vaibs <vaibhavmuratkar2@gmail.com>
2021-12-12 12:04:25 +0530

    Added empCheck UC1

--- src/com/bridgelabz/basiccoreprogram/Empwage.java
+++ src/com/bridgelabz/basiccoreprogram/Empwage.java
@@ -2,6 +2,13 @@ package com.bridgelabz.basiccoreprogram;
 
 public class Empwage {
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        //Constants
+        int IS_FULL_TIME = 1;
+        //Computation
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if(empCheck == IS_FULL_TIME)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
     }
 }
